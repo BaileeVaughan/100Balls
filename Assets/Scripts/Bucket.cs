65df1e40032b774a7960ff1ea286474a22983465
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-06-20 01:35:07 +1000

    Initial commit

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    private Rigidbody2D rigid;
+    private Renderer[] renderers;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+    void Update()
+    {
+        HandlePosition();
+        HandleBoundary();
+    }
+
+    void HandlePosition()
+    {
+        rigid.velocity = Vector3.left * movementSpeed;
+    }
+
+    void HandleBoundary()
+    {
+        Vector3 transformPos = transform.position;
+        Vector3 viewportPos = Camera.main.WorldToScreenPoint(transformPos);
+        if (IsVisible() == false && viewportPos.x < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    bool IsVisible()
+    {
+        foreach (var renderer in renderers)
+        {
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Paddles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddles : MonoBehaviour
+{
+    public Animator paddle;
+
+    private void Start()
+    {
+        paddle = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            paddle.SetBool("isOpen", true);
+        }
+        else
+        {
+            paddle.SetBool("isOpen", false);
+        }
+    }
+}
